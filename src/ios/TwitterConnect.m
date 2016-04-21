62a87cf9978db106e619fd5c5f5e7ee503b53b82
jimmy <jimmy@threefoldphotos.com>
2016-04-21 14:35:08 -0700

    add loadUserWithID to signin

--- src/ios/TwitterConnect.m
+++ src/ios/TwitterConnect.m
@@ -1,4 +1,3 @@
-
 #import <Foundation/Foundation.h>
 #import "TwitterConnect.h"
 #import <Fabric/Fabric.h>
@@ -8,12 +7,13 @@
 
 - (void)pluginInitialize
 {
-    
+
     NSString* consumerKey = [self.commandDelegate.settings objectForKey:[@"TwitterConsumerKey" lowercaseString]];
     NSString* consumerSecret = [self.commandDelegate.settings objectForKey:[@"TwitterConsumerSecret" lowercaseString]];
+
     [[Twitter sharedInstance] startWithConsumerKey:consumerKey consumerSecret:consumerSecret];
     [Fabric with:@[[Twitter sharedInstance]]];
-    
+
     [Fabric with:@[TwitterKit]];
 }
 
@@ -21,21 +21,40 @@
 - (void)login:(CDVInvokedUrlCommand*)command
 {
     [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession *session, NSError *error) {
-		CDVPluginResult* pluginResult = nil;
 		if (session){
 			NSLog(@"signed in as %@", [session userName]);
-			NSDictionary *userSession = @{
+
+            [[[Twitter sharedInstance] APIClient] loadUserWithID:[session userID]
+                                                  completion:^(TWTRUser *user,
+                                                               NSError *error)
+            {
+                if (user) {
+                    NSDictionary *userSession = @{
 										  @"userName": [session userName],
 										  @"userId": [session userID],
 										  @"secret": [session authTokenSecret],
-										  @"token" : [session authToken]};
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userSession];
-		} else {
+										  @"token" : [session authToken],
+										  @"name"  : user.name,
+										  @"userID" : user.userID,
+										  @"screenName" : user.screenName,
+										  @"profileImageURL" : user.profileImageURL,
+                                          };
+                    CDVPluginResult* pir = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userSession];
+                    [self.commandDelegate sendPluginResult:pir callbackId:command.callbackId];
+
+                } else {
+                    NSLog(@"Twitter error getting profile : %@", [error localizedDescription]);
+                    CDVPluginResult* pir = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
+                    [self.commandDelegate sendPluginResult:pir callbackId:command.callbackId];
+                }
+            }];
+
+        } else {
 			NSLog(@"error: %@", [error localizedDescription]);
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
+			CDVPluginResult* pir = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
+            [self.commandDelegate sendPluginResult:pir callbackId:command.callbackId];
 		}
-		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-	}];
+    }];
 }
 
 - (void)logout:(CDVInvokedUrlCommand*)command
